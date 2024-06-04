e25706b6b428f70314b97ce3a248e9b79e9a30db
Aceballos <ceballoscardonaalexander@gmail.com>
2024-06-04 13:23:19 -0500

    Add load

--- src/Routers/main_router.js
+++ src/Routers/main_router.js
@@ -5,12 +5,11 @@ const mainService = require("../services/main_service");
 
 router.post('/send_billing', async (req, res) => {
     
-    const data = JSON.parse(req.body); 
+    res.status(200).send("Billing received")
+    const data = req.body; 
     const new_service = new mainService();
     await new_service.resend_billings(data); 
 
-    res.status(200).send("Billing received")
-    
 })
 
 
